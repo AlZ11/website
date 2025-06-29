af525bc1b6efe86fd31bfd48addac33a74c74b42
AlZ11 <alexzhang2017@gmail.com>
2025-06-29 17:10:07 +1000

    set up app and tailwind

---
+++ alz11/src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+	return <div className='bg-blue-500 text-amber-300 text-2xl font-bold p-4'>If you see blue, Tailwind is working!</div>;
+}
+
+export default App;
