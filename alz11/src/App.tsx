793b125c047862d81cb737a18b014a5728d31243
AlZ11 <alexzhang2017@gmail.com>
2025-07-12 21:54:27 +1000

    added not finished map section

--- alz11/src/App.tsx
+++ alz11/src/App.tsx
@@ -6,16 +6,18 @@ import Hero from './pages/hero.tsx';
 import Projects from './pages/projects.tsx';
 import AuroraBackground from '../components/aurora-background.tsx';
 import Footer from '../components/Footer.tsx';
+import Map from './pages/map.tsx';
 
 function App() {
 	return (
 		<Router>
 			<AuroraBackground>
-				<Navbar options={['About', 'Projects', 'Extracurriculars']} />
+				<Navbar options={['About', 'Projects', 'Map']} />
 				<Routes>
 					<Route path='' element={<Landing />}></Route>
 					<Route path='/about' element={<Hero />}></Route>
 					<Route path='/projects' element={<Projects />}></Route>
+					<Route path='/map' element={<Map />}></Route>
 				</Routes>
 				<Footer />
 			</AuroraBackground>
---
+++ alz11/src/pages/map.tsx
@@ -0,0 +1,7 @@
+export default function Map() {
+	return (
+		<>
+			<section className='flex items-center justify-center font-mono font-bold h-screen'>Coming soon...</section>
+		</>
+	);
+}
