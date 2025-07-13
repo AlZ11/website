a883765a95adf32b5d5a6cf5f652177656bd930c
AlZ11 <alexzhang2017@gmail.com>
2025-07-13 16:37:28 +1000

    altered card properties from Acernity UI

--- alz11/src/App.tsx
+++ alz11/src/App.tsx
@@ -7,17 +7,29 @@ import Projects from './pages/projects.tsx';
 import AuroraBackground from '../components/aurora-background.tsx';
 import Footer from '../components/Footer.tsx';
 import Map from './pages/map.tsx';
+import Page from '../components/Card.tsx';
 
 function App() {
 	return (
 		<Router>
 			<AuroraBackground>
-				<Navbar options={['About', 'Projects', 'Map']} />
+				<Navbar options={['About', 'Projects', 'Map', 'Page']} />
 				<Routes>
 					<Route path='' element={<Landing />}></Route>
 					<Route path='/about' element={<Hero />}></Route>
 					<Route path='/projects' element={<Projects />}></Route>
 					<Route path='/map' element={<Map />}></Route>
+					<Route
+						path='/page'
+						element={
+							<Page className='flex flex-col items-center justify-center'>
+								<img
+									className='h-full w-full absolute inset-0 object-cover'
+									src='https://images.unsplash.com/photo-1512618831669-521d4b375f5d?q=80&w=3388&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+								/>
+							</Page>
+						}
+					></Route>
 				</Routes>
 				<Footer />
 			</AuroraBackground>
--- alz11/src/pages/projects.tsx
+++ alz11/src/pages/projects.tsx
@@ -17,6 +17,7 @@ import {
 } from 'react-icons/si';
 import { FaReact, FaPython, FaJava } from 'react-icons/fa';
 import { ImStatsDots } from 'react-icons/im';
+import Card from '../../components/Card';
 
 const ICONSIZE = 40;
 
@@ -24,7 +25,7 @@ const PROJECTS = [
 	{
 		name: 'Personal Website',
 		description:
-			'A sleek, fully-responsive portfolio site built with React, Tailwind CSS and Typescript that showcases my projects, resume, and blog in a clean, single page layout. I plan on migrating to Next.js in the near future for SEO and server-side rendering.',
+			'A sleek, fully-responsive portfolio site built with React, Tailwind CSS and Typescript that showcases my projects, resume, and blog (upcoming). I plan on migrating to Next.js in the near future for SEO and server-side rendering.',
 		imageSrc: <IoPersonCircle size={ICONSIZE} />,
 		imageAlt: 'Personal Website image',
 		href: 'https://github.com/AlZ11/website',
@@ -44,8 +45,8 @@ const PROJECTS = [
 		description:
 			'A lightweight Python toolset built with Selenium, utilised Beautiful Soup and pandas to crawl official Premier League website, to deliver clean data frames ready for analysis or ML pipelines. Deprecated due to new PL website layout change following the 25/26 season',
 		imageSrc: <GiSoccerBall size={ICONSIZE} />,
-		imageAlt: 'https://github.com/AlZ11/PL-Webscraper',
-		href: '#',
+		imageAlt: 'PL Webscraper image',
+		href: 'https://github.com/AlZ11/PL-Webscraper',
 		tools: [<SiPandas />, <SiSelenium />, <FaPython />, <SiJupyter />]
 	},
 	{
@@ -71,23 +72,23 @@ const PROJECTS = [
 export default function Projects() {
 	return (
 		<div className='text-white font-mono max-w-7xl px-4 sm:px-6 lg:px-8 mx-auto'>
-			<div className='max-w-lg py-16 sm:py-24 px-5 lg:max-w-none lg:py-32 mx-auto'>
+			<div className='max-w-lg sm:py-24 px-5 lg:max-w-none lg:py-32 mx-auto'>
 				<h2 className='text-2xl font-bold font-mono'>My projects</h2>
 
 				<div className='mt-8 space-y-12 lg:grid lg:grid-cols-3 lg:space-y-20 lg:gap-x-15'>
 					{PROJECTS.map((project) => (
 						<div key={project.name} className='group relative'>
-							<div className='flex flex-col items-start gap-y-2 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-neutral-900 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10 text-left hover:scale-110 transition duration-300 ease-in-out'>
-								<i>{project.imageSrc}</i>
-								<p className='mt-3 text-2xl font-semibold'>{project.name}</p>
-								<h3 className='mt-2 text-gray-500 text-sm'>
-									<a href={project.href}>
+							<a href={project.href}>
+								<Card className='flex flex-col items-start rounded-xl p-6 text-left hover:scale-110 transition duration-300 ease-in-out'>
+									<i>{project.imageSrc}</i>
+									<p className='mt-3 text-2xl font-semibold'>{project.name}</p>
+									<h3 className='mt-2 text-gray-500 text-sm'>
 										<span className='absolute inset-0' />
 										{project.description}
-									</a>
-								</h3>
-								<i className='flex flex-row gap-5 pt-3'>{project.tools}</i>
-							</div>
+									</h3>
+									<i className='flex flex-row gap-5 pt-3'>{project.tools}</i>
+								</Card>
+							</a>
 						</div>
 					))}
 				</div>
